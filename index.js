7b5da2e160c4c9ff9efdc5e20a114b632dd431c4
Bilshansakib <burakaghani@gmail.com>
2024-04-27 05:38:58 +0600

    find email from the craftList  activated

--- index.js
+++ index.js
@@ -45,7 +45,21 @@ async function run() {
       const result = await craftCollection.insertOne(newCraft);
       res.send(result);
     });
+
+    app.get("/craft/:email", async (req, res) => {
+      console.log(req.params.email);
+      const result = await craftCollection
+        .find({ email: req.params.email })
+        .toArray();
+      res.send(result);
+    });
     // user section
+    app.get("/user", async (req, res) => {
+      const cursor = userCollection.find();
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+
     app.post("/user", async (req, res) => {
       const user = req.body;
       console.log(user);
